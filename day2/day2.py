f14ebac4af1c657d55a0c03c6f7ceb7f8533f6cf
Martin Karlsson <xenovoyance@gmail.com>
2019-10-28 10:42:30 +0100

    Day 2 completed.

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-run_env = "test"  # test or prod
+run_env = "prod"  # test or prod
 
 if run_env == "test":
     input = "day2-inputtest.txt"
@@ -8,6 +8,7 @@ else:
     input = "day2-input.txt"
 
 totalBoxArea = 0
+totalribbon = 0
 currentExtra = 0
 
 
@@ -20,17 +21,39 @@ def smallestinlist(smallist):
     return int(smallist[0])
 
 
+def messureribbon(first, second, third):
+    # Messure both smallest distance around sides AND smallest perimeter of any one surface. Take smallest and pass on together with bow
+
+    distance = [0, 0, 0, 0]
+
+    distance[0] = 2 * first + 2 * second
+    distance[1] = 2 * first + 2 * second
+    distance[2] = 2 * second + 2 * third
+    distance[3] = 2 * first + 2 * third
+
+    distance.sort()
+
+    bow = first * second * third
+
+    return distance[0] + bow
+
+
 def surfaceAreaOfBox(boxlist):
     global currentExtra
+    global totalribbon
+
+    boxlist.sort()
+    addRibbonToTotal(messureribbon(int(boxlist[0]), int(boxlist[1]), int(boxlist[2])))
 
     firstside = int(boxlist[0]) * int(boxlist[1])
     secondside = int(boxlist[1]) * int(boxlist[2])
     thirdside = int(boxlist[0]) * int(boxlist[2])
     currentExtra = smallestinlist([firstside, secondside, thirdside])
+    #print(firstside, secondside, thirdside, ">", currentExtra)
     sides = 2 * firstside + 2 * secondside + 2 * thirdside
     total = sides + currentExtra
 
-    print(sides, "(", currentExtra, ")", ">>", total)
+    #print(sides, "(", currentExtra, ")", ">>", total, "(Ribbon:", totalribbon, ")")
 
     return total
 
@@ -40,7 +63,20 @@ def addAreaToTotal(boxarea):
     totalBoxArea += boxarea
 
 
+def addRibbonToTotal(length):
+    global totalribbon
+    totalribbon += length
+
+
 with open(input) as blockstream:
     for row in blockstream:
         addAreaToTotal(surfaceAreaOfBox(breakApartString(row)))
-print("\nThe total box area is:", totalBoxArea)
+
+print("\nThe total box area is", totalBoxArea, "and ribbon length is", totalribbon)
+
+
+'''
+Right answers:
+Wrapping paper: 1588178
+Ribbon: 3783758
+'''
