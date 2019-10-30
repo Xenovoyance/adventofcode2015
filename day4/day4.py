3bbb99ec48b2ca1bf4a866af072474e13f6e7a30
Martin Karlsson <xenovoyance@gmail.com>
2019-10-30 13:20:27 +0100

    Day 4 completed.

--- day4/day4.py
+++ day4/day4.py
@@ -8,16 +8,17 @@ iteration = 0
 
 while found_hash == False:
     new_key = secret_key + str(iteration)
-    # print(".")
-    if hashlib.md5(new_key.encode()).hexdigest()[0] == "0":
-        if hashlib.md5(new_key.encode()).hexdigest()[1] == "0":
-            if hashlib.md5(new_key.encode()).hexdigest()[2] == "0":
-                if hashlib.md5(new_key.encode()).hexdigest()[3] == "0":
-                    if hashlib.md5(new_key.encode()).hexdigest()[4] == "0":
-                        print(hashlib.md5(new_key.encode()).hexdigest() +
+    temp_hash = hashlib.md5(new_key.encode()).hexdigest()
+
+    if temp_hash[0] == "0":
+        if temp_hash[1] == "0":
+            if temp_hash[2] == "0":
+                if temp_hash[3] == "0":
+                    if temp_hash[4] == "0":
+                        print(temp_hash +
                               " found after " + str(iteration) + " tries.")
-                        if hashlib.md5(new_key.encode()).hexdigest()[5] == "0":
-                            print(hashlib.md5(new_key.encode()).hexdigest() +
+                        if temp_hash[5] == "0":
+                            print(temp_hash +
                                   " found after " + str(iteration) + " tries.")
                             break
     iteration += 1
